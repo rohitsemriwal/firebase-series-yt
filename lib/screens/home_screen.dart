11fddedb03aa35071431731076f40b3c17a42ad7
Rohit Semriwal <rohitsemriwal@gmail.com>
2022-06-05 18:17:53 +0530

    Ep. 4

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
+import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebaseseries/screens/email_auth/login_screen.dart';
 import 'package:firebaseseries/screens/phone_auth/sign_in_with_phone.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -19,6 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController ageController = TextEditingController();
+  File? profilepic;
 
   void logOut() async {
     await FirebaseAuth.instance.signOut();
@@ -28,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
     ));
   }
 
-  void saveUser() {
+  void saveUser() async {
     String name = nameController.text.trim();
     String email = emailController.text.trim();
     String ageString = ageController.text.trim();
@@ -39,11 +45,26 @@ class _HomeScreenState extends State<HomeScreen> {
     emailController.clear();
     ageController.clear();
 
-    if(name != "" && email != "") {
+    if(name != "" && email != "" && profilepic != null) {
+      
+      UploadTask uploadTask = FirebaseStorage.instance.ref().child("profilepictures").child(Uuid().v1()).putFile(profilepic!);
+
+      StreamSubscription taskSubscription = uploadTask.snapshotEvents.listen((snapshot) {
+        double percentage = snapshot.bytesTransferred/snapshot.totalBytes * 100;
+        log(percentage.toString());
+      });
+
+      TaskSnapshot taskSnapshot = await uploadTask;
+      String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+
+      taskSubscription.cancel();
+
+
       Map<String, dynamic> userData = {
         "name": name,
         "email": email,
         "age": age,
+        "profilepic": downloadUrl,
         "samplearray": [name, email, age]
       };
       FirebaseFirestore.instance.collection("users").add(userData);
@@ -52,6 +73,10 @@ class _HomeScreenState extends State<HomeScreen> {
     else{ 
       log("Please fill all the fields!");
     }
+
+    setState(() {
+      profilepic = null;
+    });
   }
 
   @override
@@ -73,6 +98,29 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.all(15),
           child: Column(
             children: [
+
+              CupertinoButton(
+                onPressed: () async {
+                  XFile? selectedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+                  if(selectedImage != null) {
+                    File convertedFile = File(selectedImage.path);
+                    setState(() {
+                      profilepic = convertedFile;
+                    });
+                    log("Image selected!");
+                  }
+                  else {
+                    log("No image selected!");
+                  }
+                },
+                padding: EdgeInsets.zero,
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: (profilepic != null) ? FileImage(profilepic!) : null,
+                  backgroundColor: Colors.grey,
+                ),
+              ),
 
               TextField(
                 controller: nameController,
@@ -124,6 +172,9 @@ class _HomeScreenState extends State<HomeScreen> {
                             Map<String, dynamic> userMap = snapshot.data!.docs[index].data() as Map<String, dynamic>;
 
                             return ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(userMap["profilepic"]),
+                              ),
                               title: Text(userMap["name"] + " (${userMap["age"]})"),
                               subtitle: Text(userMap["email"]),
                               trailing: IconButton(
